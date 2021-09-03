b3806a60d00c3a66486d5c62879a19a98441f49c
Toni Sevener <tsevener@wikimedia.org>
2021-09-03 16:49:22 -0500

    Add and assign needed RemoteNotificationType enum cases

--- WMF Framework/Remote Notifications/Model/RemoteNotification+CoreDataClass.swift
+++ WMF Framework/Remote Notifications/Model/RemoteNotification+CoreDataClass.swift
@@ -20,8 +20,60 @@ public class RemoteNotification: NSManagedObject {
 private extension RemoteNotification {
     func calculateRemoteNotificationType() -> RemoteNotificationType {
         switch (categoryString, typeString) {
-        default:
+        case ("edit-user-talk", "edit-user-talk"):
+            return .userTalkPageMessage
+        case ("edit-user-talk", "flowusertalk-new-topic"):
+            return .flowUserTalkPageNewTopic
+        case ("edit-user-talk", "flowusertalk-post-reply"):
+            return .flowUserTalkPageReply
+        case ("flow-discussion", "flow-new-topic"):
+            return .flowDiscussionNewTopic
+        case ("flow-discussion", "flow-post-reply"):
+            return .flowDiscussionReply
+        case ("mention", "mention"):
+            return .mentionInTalkPage
+        case ("mention", "mention-summary"):
+            return .mentionInEditSummary
+        case ("mention", "flow-mention"):
+            return .flowMention
+        case ("mention", "flow-mention"):
+            return .flowMention
+        case ("reverted", "reverted"):
             return .editReverted
+        case ("user-rights", "user-rights"):
+            return .userRightsChange
+        case ("page-review", "pagetriage-mark-as-reviewed"):
+            return .pageReviewed
+        case ("article-linked", "page-linked"):
+            return .pageLinked
+        case ("wikibase-action", "page-connection"):
+            return .connectionWithWikidata
+        case ("edit-thank", "edit-thank"):
+            return .thanks
+        case ("edit-thank", "flow-thank"):
+            return .flowThanks
+        case ("cx", "cx-first-translation"):
+            return .translationMilestone(1)
+        case ("cx", "cx-tenth-translation"):
+            return .translationMilestone(10)
+        case ("cx", "cx-hundredth-translation"):
+            return .translationMilestone(100)
+        case ("thank-you-edit", "thank-you-edit"):
+            return .editMilestone
+        case ("system-noemail", "welcome"):
+            return .welcome
+        case ("login-fail", "login-fail-new"):
+            return .loginFailUnknownDevice
+        case ("login-fail", "login-fail-known"):
+            return .loginFailKnownDevice
+        case ("login-success", "login-success"):
+            return .loginSuccessUnknownDevice
+        case ("system", _),
+             ("system-noemail", _),
+             ("system-emailonly", _):
+            return .unknownSystem
+        default:
+            return .unknown
         }
     }
 }
--- WMF Framework/Remote Notifications/Model/RemoteNotificationType.swift
+++ WMF Framework/Remote Notifications/Model/RemoteNotificationType.swift
@@ -1,6 +1,27 @@
 import Foundation
 
 public enum RemoteNotificationType {
+    case userTalkPageMessage
+    case flowUserTalkPageNewTopic
+    case flowUserTalkPageReply
+    case flowDiscussionNewTopic
+    case flowDiscussionReply
+    case mentionInTalkPage
+    case mentionInEditSummary
+    case flowMention
     case editReverted
+    case userRightsChange
+    case pageReviewed
+    case pageLinked
+    case connectionWithWikidata
+    case thanks
+    case flowThanks
+    case translationMilestone(Int)
+    case editMilestone
+    case welcome
+    case loginFailUnknownDevice
+    case loginFailKnownDevice
+    case loginSuccessUnknownDevice
+    case unknownSystem
     case unknown
 }
